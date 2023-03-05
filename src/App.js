ca32cccd4eb258e1c87f24eab4dedd8ac5ec077c
Jared <jarednuguid@gmail.com>
2023-03-05 14:39:41 -0500

    Import and add assets to cards

--- src/App.js
+++ src/App.js
@@ -5,20 +5,34 @@ import Cards from './components/Cards.js';
 import React, { useState, useEffect } from 'react';
 import uniqid from 'uniqid';
 
+// Card Images
+import ace from './assets/ace.webp';
+import cleo from './assets/cleo.webp';
+import egbert from './assets/egbert.webp';
+import filbert from './assets/filbert.webp';
+import flo from './assets/flo.webp';
+import octavian from './assets/octavian.webp';
+import peewee from './assets/peewee.webp';
+import raymond from './assets/peewee.webp';
+import rudy from './assets/rudy.webp';
+import tank from './assets/tank.webp';
+import wolfgang from './assets/wolfgang.webp';
+import yuka from './assets/yuka.webp';
+
 function App() {
   const cardsArray = [
-    { name: 'Filbert', id: uniqid(), },
-    { name: 'Flo', id: uniqid(), },
-    { name: 'Peewee', id: uniqid(), },
-    { name: 'Raymond', id: uniqid(), },
-    { name: 'Rudy', id: uniqid(), },
-    { name: 'Tank', id: uniqid(), },
-    { name: 'Wolfgang', id: uniqid(), },
-    { name: 'Yuka', id: uniqid(), },
-    { name: 'Egbert', id: uniqid(), },
-    { name: 'Cleo', id: uniqid(), },
-    { name: 'Ace', id: uniqid(), },
-    { name: 'Octavian', id: uniqid(), },
+    { name: 'Filbert', id: uniqid(), image: {filbert} },
+    { name: 'Flo', id: uniqid(), image: {flo} },
+    { name: 'Peewee', id: uniqid(), image: {peewee} },
+    { name: 'Raymond', id: uniqid(), image: {raymond} },
+    { name: 'Rudy', id: uniqid(), image: {rudy} },
+    { name: 'Tank', id: uniqid(), image: {tank} },
+    { name: 'Wolfgang', id: uniqid(), image: {wolfgang} },
+    { name: 'Yuka', id: uniqid(), image: {yuka} },
+    { name: 'Egbert', id: uniqid(), image: {egbert} },
+    { name: 'Cleo', id: uniqid(), image: {cleo} },
+    { name: 'Ace', id: uniqid(), image: {ace} },
+    { name: 'Octavian', id: uniqid(), image: {octavian} },
   ]
 
   const [score, setScore] = useState(0);
